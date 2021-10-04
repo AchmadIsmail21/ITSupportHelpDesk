50d4c31680cc28b60e26be7daccbac9fd0532a55
Parlinggoman <parlinggomanpakpahan055@gmail.com>
2021-10-04 11:40:21 +0700

    LoginVMRegisterVM

--- ITSupportHelpDesk/API/ViewModel/LoginVM.cs
+++ ITSupportHelpDesk/API/ViewModel/LoginVM.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using API.Model;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +9,21 @@ namespace API.ViewModel
 {
     public class LoginVM
     {
+        public string Id { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Password Harus di Isi")]
+        // [MinLength(6,ErrorMessage = "Password Minimal 6 Digit")]
+        public string Password { get; set; }
+
+
+        // [Required(ErrorMessage = "New Password Harus di Isi Yang Baru")]
+        //[MinLength(6,ErrorMessage = "Password Minimal 6 Digit")]
+        public string NewPassword { get; set; }
+        public string OTP { get; set; }
+        public int RoleId { get; set; }
+
+        public virtual Role Roles { get; set; }
     }
 }
--- ITSupportHelpDesk/API/ViewModel/RegisterVM.cs
+++ ITSupportHelpDesk/API/ViewModel/RegisterVM.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +10,45 @@ namespace API.ViewModel
 {
     public class RegisterVM
     {
+       
+        [Required(ErrorMessage = "Id Harus di Isi")]
+        // [StringLength(9, ErrorMessage = "NIK Harus Terdiri Dari 9 Digit ")]
+        public string Id { get; set; }
+        [Required(ErrorMessage = "First Harus di Isi")]
+        public string FirstName { get; set; }
+        [Required(ErrorMessage = "Last Name Harus di Isi")]
+        public string LastName { get; set; }
+        [Phone(ErrorMessage = "Phone Number Name Harus di Isi")]
+        // [StringLength(12,ErrorMessage ="PhoneNumber Harus Terdiri Dari 12 Digit")]
+        public string Phone { get; set; }
+        [Required(ErrorMessage = "Company Harus di Isi")]
+        public string Department { get; set; }
+        [Required(ErrorMessage = "Company Harus di Isi")]
+        public string Company { get; set; }
+        [Required(ErrorMessage = "BirthDate Harus diisi")]
+        public DateTime BirthDate { get; set; }
+
+
+        public enum Gender
+        {
+            Male,
+            Female
+        }
+        public Gender gender { get; set; }
+        [Required(ErrorMessage = "Salary Depan tidak boleh kosong")]
+        public int Salary { get; set; }
+        [Required(ErrorMessage = "Email tidak boleh kosong"),
+       EmailAddress(ErrorMessage = "Alamat Email tidak valid")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password tidak boleh kosong")]
+        // MinLength(8, ErrorMessage = "Password Minimal 8 Karakter"),
+        // RegularExpression("^(?=[^a-z]*[a-z])(?=[^A-Z]*[A-Z])(?=\\D*\\d)[^:&.~\\s]{5,20}$", ErrorMessage = "Password harus mengadung huruf besar,huruf kecil dan angka ")]
+        public string Password { get; set; }
+       
+        [Required(ErrorMessage = "RoleId tidak boleh kosong")]
+        public int RoleId { get; set; }
+
+   
     }
 }
