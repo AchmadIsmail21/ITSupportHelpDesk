9a9e87f5106b67b5e31553715217d035c6e843c3
Achmad <achmadismail423@gmail.com>
2021-10-04 13:59:20 +0700

    Controller

---
+++ ITSupportHelpDesk/API/Controllers/StaffCasesController.cs
@@ -0,0 +1,24 @@
+﻿using API.Base;
+using API.Model;
+using API.Repository.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StaffCasesController : BaseController<StaffCase, StaffCaseRepository, int>
+    {
+        private readonly StaffCaseRepository staffCaseRepository;
+
+        public StaffCasesController(StaffCaseRepository staffCaseRepository) : base(staffCaseRepository)
+        {
+            this.staffCaseRepository = staffCaseRepository;
+        }
+    }
+}
