324436dc2c6306c4a5edf666d9a0475704043283
Achmad <achmadismail423@gmail.com>
2021-10-06 15:09:20 +0700

    CaseRepo-Convertation

--- ITSupportHelpDesk/API/Controllers/ConvertationsController.cs
+++ ITSupportHelpDesk/API/Controllers/ConvertationsController.cs
@@ -1,6 +1,7 @@
 ﻿using API.Base;
 using API.Model;
 using API.Repository.Data;
+using API.ViewModel;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
@@ -19,6 +20,72 @@ namespace API.Controllers
         public ConvertationsController(ConvertationRepository convertationRepository) : base(convertationRepository)
         {
             this.convertationRepository = convertationRepository;
+        }
+
+        [HttpPost("CreateConvertations")]
+        public ActionResult CreateConvertation(CreateConvertationVM createConvertationVM) {
+            var create = convertationRepository.CreateConvertation(createConvertationVM);
+            if (create > 0)
+            {
+                return Ok("Anda Berhasil membuat tiket anda");
+            }
+            else {
+                return BadRequest("Gagal membuat tiket");
+            }
+        }
+
+        [HttpGet("GetConvertations")]
+        public ActionResult GetConvertations() {
+            var get = convertationRepository.GetConvertations();
+            if (get != null)
+            {
+                return Ok(get);
+            }
+            else {
+                return BadRequest("Percakapan tidak ditemukan");
+            }
+        }
+
+        [HttpGet("ViewConvertations")]
+        public ActionResult ViewConvertations() {
+            var getConvertation = convertationRepository.ViewConvertation();
+            if (getConvertation != null)
+            {
+                return Ok(getConvertation);
+            }
+            else {
+                return BadRequest("Percakapan Tidak ditemukan");
+            }
+        }
+
+        [HttpGet("ViewConvertationCaseId/{id}")]
+        public ActionResult ViewConvertationsByCaseId(int id)
+        {
+            var getConvertationByCaseId = convertationRepository.ViewConvertationCaseId(id);
+            if (getConvertationByCaseId != null)
+            {
+                return Ok(getConvertationByCaseId);
+            }
+            else
+            {
+                return BadRequest("Percakapan dengan id kasus tersebut Tidak Ditemukan");
+            }
+
+        }
+
+        [HttpGet("ViewConvertationUserId/{id}")]
+        public ActionResult ViewConvertationsByUserId(int id)
+        {
+            var getUserId = convertationRepository.ViewConvertationUserId(id);
+            if (getUserId != null)
+            {
+                return Ok(getUserId);
+            }
+            else
+            {
+                return BadRequest("Data Tidak Ditemukan");
+            }
+
         }
     }
 }
---
+++ ITSupportHelpDesk/API/ViewModel/ConvertationVM.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.ViewModel
+{
+    public class ConvertationVM
+    {
+        public int Id { get; set; }
+        public DateTime DateTime { get; set; }
+        public string Message { get; set; }
+        public string CaseName { get; set; }
+        public int CaseId { get; set; }
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+        
+    }
+}
